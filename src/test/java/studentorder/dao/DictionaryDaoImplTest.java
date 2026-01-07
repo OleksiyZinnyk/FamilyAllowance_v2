f3efac218d7612275c766e85f0db1b3d20b41c14
OleksiyZinnyk <zniniklehaa@gmail.com>
2026-01-07 12:55:49 +0200

    start Unit testing

---
+++ src/test/java/studentorder/dao/DictionaryDaoImplTest.java
@@ -0,0 +1,23 @@
+package studentorder.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DictionaryDaoImplTest {
+
+    @Test
+    public void testExample1(){
+        System.out.println("Test 1");
+    }
+
+    @Test
+    public void testExample2(){
+        System.out.println("Test 2");
+    }
+
+    @Test
+    public void testExample3(){
+        System.out.println("Test 3");
+    }
+}
